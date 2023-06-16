7778d1043d270856a3355ddaff19a8db0f71b206
Christine Baxter <christine.d.baxter@live.com>
2023-06-15 23:11:23 -0500

    added content to new cards, fixed nav links, updated CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,42 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+// Add click event listener using event delegation
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('collapsible')) {
+    event.target.classList.toggle('active');
+    var content = event.target.nextElementSibling;
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
     } else {
-      content.style.display = "block";
+      content.style.display = 'block';
     }
+  }
+});
+
+// Add smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const headerHeight = document.querySelector('header').offsetHeight; // Get the height of the sticky header
+    const target = document.querySelector(this.getAttribute('href'));
+    const targetPosition = target.offsetTop - headerHeight; // Adjust the target position by subtracting the header height
+
+    window.scrollTo({
+      top: targetPosition,
+      behavior: 'smooth'
+    });
   });
-}
+});
+
+
+// JavaScript code for sticky header
+window.addEventListener('scroll', function() {
+  var section = document.querySelector('section');
+  var header = document.querySelector('header');
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 0) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+});
