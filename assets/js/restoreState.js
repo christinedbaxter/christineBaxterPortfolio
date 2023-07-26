a31f386a9633a8b1fef804c826d2197a0af786bf
Christine Baxter <christine.d.baxter@live.com>
2023-07-26 16:35:12 -0500

    updated collapsible functionality

--- assets/js/restoreState.js
+++ assets/js/restoreState.js
@@ -1,28 +1,58 @@
 // Store the state variables
 let isProjectTypeExpanded = false;
 let activeProjectCard = null;
+let activeProjectType = null;
 
 // Get the required elements from the DOM
-const projectTypeSection = document.getElementById('projTypeName');
-const projectTypeToggle = document.getElementById('projTypeToggle');
-const projectCards = document.getElementsByClassName('card');
+const projectTypeSection = document.getElementById('myProjectsRow');
+const projectTypeAnalytics = document.getElementById('dataAnalytics');
+const projectTypeFullStack = document.getElementById('fullStackWebDev');
+const projectTypeToggle = document.getElementsByClassName('projType');
+const projectCards = document.getElementsByClassName('card-container');
 
-// Function to expand/collapse Data Analytics section
-function toggleProjectTypes() {
+// Function to expand/collapse Data Analytics/Full Stack Web Development sections
+function toggleProjectTypes(projTypeIndex) {
+    activeProjectType = projTypeIndex;
     isProjectTypeExpanded = !isProjectTypeExpanded;
     projectTypeSection.classList.toggle('expanded', isProjectTypeExpanded);
+    }
 
     // Should be null because we haven't modified the history stack yet
     console.log("History.state before pushState: ", history.state);
     
     // Store the current state in history
     history.pushState({ isProjectTypeExpanded: isProjectTypeExpanded, activeProjectCard: activeProjectCard }, "");
-}
+
 
 // Function to handle project card click
 function handleProjectCardClick(cardIndex) {
     activeProjectCard = cardIndex;
+    projectCards[cardIndex].scrollIntoView({ behavior: 'smooth' });
+
     // Perform necessary actions when a project card is clicked
+    let coll = document.getElementsByClassName("collapsible");
+    
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            let content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+    // When the page is loaded...
+    window.onload = function() {
+        for (let i = 0; i < coll.length; i++) {
+            if (localStorage.getItem(coll[i].id) == 'expanded') {
+                coll[i].classList.add("active");
+                let content = coll[i].nextElementSibling;
+                content.style.display = "block";
+            }
+        }
+    }
 
     // Store the current state in history
     history.pushState({ isProjectTypeExpanded: isProjectTypeExpanded, activeProjectCard: activeProjectCard }, "");
@@ -48,10 +78,25 @@ function restorePreviousState(state) {
     }
 }
 
-// Event listener for Data Analytics toggle
-projectTypeToggle.addEventListener('click', toggleProjectTypes);
+// Event listener for Data Analytics/Full Stack Web development toggles
+for (let i = 0; i < projectTypeToggle.length; i++) {
+    projectTypeToggle[i].addEventListener('click', () => toggleProjectTypes(i));
+}
 
 // Event listeners for project card clicks
 for (let i = 0; i < projectCards.length; i++) {
     projectCards[i].addEventListener('click', () => handleProjectCardClick(i));
 }
+
+window.addEventListener("popstate", (event) => {
+  console.log(
+    `location: ${document.location}, state: ${JSON.stringify(event.state)}`,
+  );
+});
+
+// Event listener for popstate (back/forward navigation)
+window.addEventListener('popstate', (event) => {
+    restorePreviousState(event.state);
+});
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,3 @@
-// Add click event listener using event delegation
-document.addEventListener('click', function (event) {
-  if (event.target.classList.contains('collapsible')) {
-    event.target.classList.toggle('active');
-    var content = event.target.nextElementSibling;
-    if (content.style.display === 'block') {
-      content.style.display = 'none';
-    } else {
-      content.style.display = 'block';
-    }
-  }
-});
-
 // JavaScript code for sticky header
 window.addEventListener('scroll', function() {
   var header = document.querySelector('header');
@@ -40,55 +27,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-window.addEventListener("popstate", (event) => {
-  console.log(
-    `location: ${document.location}, state: ${JSON.stringify(event.state)}`,
-  );
+//Add click event listener using event delegation
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('collapsible')) {
+    event.target.classList.toggle('active');
+    var content = event.target.nextElementSibling;
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
+  }
 });
-
-// Event listener for popstate (back/forward navigation)
-window.addEventListener('popstate', (event) => {
-    restorePreviousState(event.state);
-});
-
-// let coll = document.getElementsByClassName("collapsible");
-// for (let i = 0; i < coll.length; i++) {
-//     coll[i].addEventListener("click", function() {
-//         this.classList.toggle("active");
-//         let content = this.nextElementSibling;
-//         if (content.style.display === "block") {
-//             content.style.display = "none";
-//         } else {
-//             content.style.display = "block";
-//         }
-//     });
-// }
-
-// // When a collapsible is clicked...
-// coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     let content = this.nextElementSibling;
-//     if (content.style.display === "block") {
-//         content.style.display = "none";
-//         // Remove from localStorage when the section is collapsed
-//         localStorage.removeItem(this.id);
-//     } else {
-//         content.style.display = "block";
-//         // Add to localStorage when the section is expanded
-//         localStorage.setItem(this.id, 'expanded');
-//     }
-// });
-
-// // When the page is loaded...
-// window.onload = function() {
-//     for (let i = 0; i < coll.length; i++) {
-//         if (localStorage.getItem(coll[i].id) == 'expanded') {
-//             coll[i].classList.add("active");
-//             let content = coll[i].nextElementSibling;
-//             content.style.display = "block";
-//         }
-//     }
-// }
 
 // window.addEventListener('DOMContentLoaded', () => {
 //     const filterButtons = document.querySelectorAll('#filter li');
